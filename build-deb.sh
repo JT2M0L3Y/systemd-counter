b6e10aabb44cc2c45ec3c72290e06749d2a75f52
JT2M0L3Y <jtsmoley@icloud.com>
2024-02-28 17:52:43 -0800

    rm comments, cp scripts

--- build-deb.sh
+++ build-deb.sh
@@ -4,25 +4,30 @@ TEMP_DIR="temp"
 
 echo "Starting deb package build"
 
-echo "Creating temporary directory tree"
+echo "Creating temp directory"
 mkdir -p $TEMP_DIR
 mkdir -p $TEMP_DIR/DEBIAN
 mkdir -p $TEMP_DIR/usr/bin
 mkdir -p $TEMP_DIR/lib/systemd/system
 
-echo "Creating control file for DEBIAN/"
 cp DEBIAN/control $TEMP_DIR/DEBIAN/control
-chmod 0775 $TEMP_DIR/DEBIAN/control
 
-echo "Copying binary into place"
 cp bin/counter $TEMP_DIR/usr/bin/
 
-echo "Copying service file into place"
 cp counter.service $TEMP_DIR/lib/systemd/system/
+
+cp DEBIAN/postinst $TEMP_DIR/DEBIAN/
+chmod 0775 $TEMP_DIR/DEBIAN/postinst
+
+cp DEBIAN/prerm $TEMP_DIR/DEBIAN/
+chmod 0775 $TEMP_DIR/DEBIAN/prerm
+
+cp DEBIAN/postrm $TEMP_DIR/DEBIAN/
+chmod 0775 $TEMP_DIR/DEBIAN/postrm
 
 echo "Building deb file"
 dpkg-deb --root-owner-group --build $TEMP_DIR
 mv $TEMP_DIR.deb counter-v2.0.0.deb
 rm -rf $TEMP_DIR
 
-echo "Complete."
+echo "Completed deb build."
